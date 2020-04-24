6a181deca7aa80fa5defe30c9c1f115e61a8376e
melo_gustavo <gustavomelo95@gmail.com>
2020-04-24 18:39:58 -0300

    criacao pagina login

--- gobarber-frontend/src/App.tsx
+++ gobarber-frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import Globalstyle from './styles/global';
+import SigniN from './pages/Signin';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello world</h1>
+    <SigniN />
     <Globalstyle />
   </>
 );
