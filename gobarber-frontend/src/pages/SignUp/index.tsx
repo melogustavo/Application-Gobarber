16c8f8f5a2e41e4dda634c963d7510c3eac668b2
melo_gustavo <gustavomelo95@gmail.com>
2020-04-27 15:10:25 -0300

    validando dados com yup

--- gobarber-frontend/src/pages/SignUp/index.tsx
+++ gobarber-frontend/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logo from '../../assets/logo.svg';
 
@@ -9,9 +10,23 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatorio'),
+        email: Yup.string()
+          .required('Email obrigatorio')
+          .email('Digite um e-mail valido'),
+        password: Yup.string().min(6, 'Senha precisa ter no minimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
