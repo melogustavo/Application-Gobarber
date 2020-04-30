1791e166becc6c9936eaf547196217f94bbff2e6
melo_gustavo <gustavomelo95@gmail.com>
2020-04-30 10:13:05 -0300

    animando o toast com react-spring

--- gobarber-frontend/src/components/ToastContainer/index.tsx
+++ gobarber-frontend/src/components/ToastContainer/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 
 import Toast from './Toast';
-import { ToastMessage, useToast } from '../../context/ToastContext';
+import { ToastMessage } from '../../context/ToastContext';
 import { Container } from './styles';
 
 interface ToastContainerProps {
@@ -9,12 +10,20 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
+  const messagesWithTransitions = useTransition(
+    messages,
+    (message) => message.id,
+    {
+      from: { right: '-120%' },
+      enter: { right: '0%' },
+      leave: { right: '-120%' },
+    },
+  );
 
   return (
     <Container>
-      {messages.map((message) => (
-        <Toast key={message.id} message={message} />
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
       ))}
     </Container>
   );
