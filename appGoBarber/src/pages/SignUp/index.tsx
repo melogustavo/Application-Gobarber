d66d3a8c427b6f7a8d0b044beb6e24cddbac9070
melo_gustavo <gustavomelo95@gmail.com>
2020-05-05 11:51:37 -0300

    cadastro de usuario na aplicacao

--- appGoBarber/src/pages/SignUp/index.tsx
+++ appGoBarber/src/pages/SignUp/index.tsx
@@ -14,6 +14,8 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
 import * as Yup from 'yup';
 
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
@@ -50,8 +52,13 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
-      // history.push('/');
+      await api.post('/users', data);
+
+      Alert.alert(
+        'Cadastro realizado com sucesso',
+        'Você já pode fazer login na aplicação',
+      );
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
