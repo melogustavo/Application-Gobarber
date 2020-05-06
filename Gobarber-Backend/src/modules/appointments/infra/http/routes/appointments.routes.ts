f22578ce0b4789b4b02e3c6e5c4250c8987b0dd7
melo_gustavo <gustavomelo95@gmail.com>
2020-05-06 10:06:36 -0300

    adicionando o depency inversion principal

--- Gobarber-Backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ Gobarber-Backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,20 +1,21 @@
-import { getCustomRepository } from 'typeorm';
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentsService from '@modules/appointments/services/CreateAppointmentService';
+
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
 // Isso significa que todas as rotas aqui vao ter que usar o token
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentRepository.find();
-  return response.json(appointments);
-});
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentRepository.find();
+//   return response.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
@@ -22,7 +23,9 @@ appointmentsRouter.post('/', async (request, response) => {
   const parsedDate = parseISO(date);
 
   // Aqui vc nao precisa instanciar o repositorio porque o service ja tem acesso a ele
-  const createAppointment = new CreateAppointmentsService();
+  const createAppointment = new CreateAppointmentsService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
--- Gobarber-Backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ Gobarber-Backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,25 +1,24 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppError from '@shared/errors/AppError';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
+import IappointmentsRepository from '../repositories/IAppointmentsRepository';
 
-interface RequestDTO {
+interface IRequestDTO {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
+  constructor(private appointmentsRepository: IappointmentsRepository) {}
+
   public async execute({
     provider_id,
     date,
-  }: RequestDTO): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-
+  }: IRequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -27,12 +26,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
