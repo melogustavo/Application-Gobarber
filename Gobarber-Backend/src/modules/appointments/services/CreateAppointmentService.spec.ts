ee7e6112a89b30bbcfd12e205c51a79e80d02b67
melo_gustavo <gustavomelo95@gmail.com>
2020-05-09 19:26:08 -0300

    add coverage e finalizacao testes de appointment

--- Gobarber-Backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ Gobarber-Backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,1 +1,42 @@
+import FakeAppointmentRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentsService from '@modules/appointments/services/CreateAppointmentService';
+
+import AppError from '@shared/errors/AppError';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date();
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123456',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
