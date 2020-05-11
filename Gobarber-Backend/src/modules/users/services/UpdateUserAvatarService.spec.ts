42035c322010f582334b860fb3e53cb8c90fa1ab
melo_gustavo <gustavomelo95@gmail.com>
2020-05-11 15:06:32 -0300

    testes de atualizacao de avatar

---
+++ Gobarber-Backend/src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,78 @@
+import AppError from '@shared/errors/AppError';
+import FakeUserRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('Update User avatar', () => {
+  it('should add avatar to existing user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      fakeUserRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUserRepository.create({
+      email: 'gustavo@email.com',
+      name: 'gustavo',
+      password: '123456',
+    });
+
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.png',
+    });
+
+    expect(user.avatar).toBe('avatar.png');
+  });
+
+  it('should not add avatar to non-existing user', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      fakeUserRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatarService.execute({
+        user_id: '123',
+        avatarFilename: 'avatar.png',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should delete old avatar when updating new one', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    // Vc esta espionando o metodo pra verificar se ele foi acionado ou nao... nesse caso vc precisa disso porque nao tem como vc verificar se o deletefile foi acionado
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const updateUserAvatarService = new UpdateUserAvatarService(
+      fakeUserRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUserRepository.create({
+      email: 'gustavo@email.com',
+      name: 'gustavo',
+      password: '123456',
+    });
+
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.png',
+    });
+
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar2.png',
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.png');
+    expect(user.avatar).toBe('avatar2.png');
+  });
+});
