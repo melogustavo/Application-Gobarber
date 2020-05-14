3190f7dbf8aa64772e0c31dc25b263380c92894b
melo_gustavo <gustavomelo95@gmail.com>
2020-05-14 09:01:35 -0300

    conectando mongo e criando estrutura de notificacoes

---
+++ Gobarber-Backend/src/modules/notifications/infra/typeorm/repositories/NotificatiosRepository.ts
@@ -0,0 +1,31 @@
+import { getMongoRepository, MongoRepository } from 'typeorm';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+
+import Notification from '../schemas/Notification';
+
+class NotificationsRepository implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    // Sempre que for fazer algum tipo de operacao que nao for a default, vc precisa passar o nome da conexao que vc definiu la no arquivo orm config para ele.
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({
+      content,
+      recipient_id,
+    });
+
+    await this.ormRepository.save(notification);
+
+    return notification;
+  }
+}
+
+export default NotificationsRepository;
